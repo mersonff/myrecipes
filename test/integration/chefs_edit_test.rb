589701280b0ce81b2102748728f3aacd19af085b
Emerson Fernandes de Freitas <mersonsi@gmail.com>
2017-09-06 23:52:57 -0300

    Completando sistema de autenticação com admin com testes

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -5,9 +5,13 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
 		@chef = Chef.create!(chefname: "Emerson", email:"emerson@example.com",
 			               password: "password", password_confirmation: "password")
+		@chef2 = Chef.create!(chefname: "john", email: "john@example.com",
+                    password: "password", password_confirmation: "password" )
+    @admin_user = Chef.create!(chefname: "john", email: "admin@example.com",
+                    password: "password", password_confirmation: "password", admin: true )
 	end
 
-  test "reject an invalid signup" do
+  test "reject an invalid edit" do
   	sign_in_as(@chef, "password")
   	get edit_chef_path(@chef)
   	assert_template 'chefs/edit'
@@ -17,7 +21,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   	assert_select 'div.panel-body'
   end
 
-  test "accept valid signup" do
+  test "accept valid edit" do
   	sign_in_as(@chef, "password")
   	get edit_chef_path(@chef)
   	assert_template 'chefs/edit'
@@ -28,5 +32,29 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   	assert_match "João", @chef.chefname
   	assert_match "emerson@test.com", @chef.email
   end
+
+  test "accept edit attempt by admin user" do
+  	sign_in_as(@admin_user, "password")
+  	get edit_chef_path(@chef)
+  	assert_template 'chefs/edit'
+  	patch chef_path(@chef), params: { chef: { chefname: "João2", email: "emerson2@test.com"} }
+  	assert_redirected_to @chef
+  	assert_not flash.empty?
+  	@chef.reload
+  	assert_match "João2", @chef.chefname
+  	assert_match "emerson2@test.com", @chef.email
+  end
+
+  test "redirect edit attempt by another non-admin user" do
+  	sign_in_as(@chef2, "password")
+  	updated_name = "joe"
+  	update_email = "joe@example.com"
+  	patch chef_path(@chef), params: { chef: { chefname: updated_name, email: update_email} }
+  	assert_redirected_to chefs_path
+  	assert_not flash.empty?
+  	@chef.reload
+  	assert_match "Emerson", @chef.chefname
+  	assert_match "emerson@example.com", @chef.email
+	end
 
 end
--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -6,7 +6,9 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
     @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com",
                     password: "password", password_confirmation: "password")
     @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
-                    password: "password", password_confirmation: "password")
+                    password: "password", password_confirmation: "password" )
+    @admin_user = Chef.create!(chefname: "john", email: "admin@example.com",
+                    password: "password", password_confirmation: "password", admin: true )
   end
 
   test "should get chefs listing" do
@@ -17,11 +19,11 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
   end
 
   test "should delete chef" do
-  	sign_in_as(@chef, "password")
+  	sign_in_as(@admin_user, "password")
   	get chefs_path
   	assert_template 'chefs/index'
   	assert_difference 'Chef.count', -1 do
-  		delete chef_path(@chef)
+  		delete chef_path(@chef2)
   	end
   	assert_redirected_to chefs_path
   	assert_not flash.empty?
